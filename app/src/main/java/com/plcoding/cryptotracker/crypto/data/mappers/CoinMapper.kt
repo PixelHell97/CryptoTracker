6a0eec1f52c71b6a6a629961f148c185b0ac3f83
Pixelh_97 <abakhet2010@gmail.com>
2024-12-23 21:46:55 +0200

    +LoadingCoinPriceHistory

--- app/src/main/java/com/plcoding/cryptotracker/crypto/data/mappers/CoinMapper.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/data/mappers/CoinMapper.kt
@@ -1,7 +1,11 @@
 package com.plcoding.cryptotracker.crypto.data.mappers
 
 import com.plcoding.cryptotracker.crypto.data.network.dto.CoinDto
+import com.plcoding.cryptotracker.crypto.data.network.dto.CoinPriceDto
 import com.plcoding.cryptotracker.crypto.domain.Coin
+import com.plcoding.cryptotracker.crypto.domain.CoinPrice
+import java.time.Instant
+import java.time.ZoneId
 
 fun CoinDto.toCoin(): Coin =
     Coin(
@@ -12,4 +16,13 @@ fun CoinDto.toCoin(): Coin =
         priceUsd = priceUsd,
         marketCapUsd = marketCapUsd,
         changePercent24Hr = changePercent24Hr,
+    )
+
+fun CoinPriceDto.toCoinPrice(): CoinPrice =
+    CoinPrice(
+        priceUsd = priceUsd,
+        dateTime =
+            Instant
+                .ofEpochMilli(time)
+                .atZone(ZoneId.of("UTC")),
     )
